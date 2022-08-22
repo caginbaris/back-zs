109e218a1d95ef4eef4dd72435531c0ad2084545
DESKTOP-6802SPQ\cagin <caginbaris@gmail.com>
2022-08-23 01:04:02 +0300

    dq filtering added but not checked

---
+++ MDK-ARM/User/Src/dqFiltering.c
@@ -0,0 +1,38 @@
+
+#include "mlib.h"
+#include "clib.h"
+#include "controlRoutines.h"
+
+/*
+%notch filter
+[bn,an]=butter(1,[90 111.2]/(fs*0.5),'stop');
+*/
+
+
+static float sosCoefficentsNotch[]={
+	
+	 0.993383790867002f,  -1.982843956421446f,   0.993383790867002f,
+  -1.982843956421515f,   0.986767581734073f
+
+};
+
+float fofCoefficents150hz[2]={
+
+0.045035005911131f,
+-0.909929988177738f
+
+};
+
+//example usage -- dqFiltering(pV,&pVf,intervalue(array with 2 elements),sosP(sos array with 2 elements),fofP(array with 2 elements)
+
+void dqFiltering(park in, park* out,float* interValue,sos_parameters* sosP, float* fofP){
+
+
+interValue[0]=sos_implementation(in.d,interValue[0],sosCoefficentsNotch,&sosP[0]);
+out->d=fof_implementation(interValue[0],&fofP[0],out->d,fofCoefficents150hz); 
+
+interValue[1]=sos_implementation(in.q,interValue[1],sosCoefficentsNotch,&sosP[1]);
+out->q=fof_implementation(interValue[1],&fofP[1],out->q,fofCoefficents150hz); 
+
+}
+
